cd3802241b8cd89392910c5a4e50f290c5d01566
Binh <54803422+KR913@users.noreply.github.com>
2021-11-15 23:12:01 +0700

    Fix Debug

--- Magical Wheel/Assets/Script/Socket/SocketDebug.cs
+++ Magical Wheel/Assets/Script/Socket/SocketDebug.cs
@@ -9,13 +9,15 @@ public static class SocketDebug
     public static void Log(string msg)
     {
         string fileName = @"\Debug.txt";
-        if (File.Exists(fileName))
+        if (!File.Exists(fileName))
         {
-            File.WriteAllText(DateTime.Now + ": " + fileName, msg + "\n\n");
+            Debug.Log("Create");
+            File.WriteAllText(fileName, DateTime.Now + ": " + msg + "\n\n");
         }
         else
         {
-            File.AppendAllText(DateTime.Now + ": " + fileName, msg + "\n\n");
+            Debug.Log("Append");
+            File.AppendAllText(fileName, DateTime.Now + ": " + msg + "\n\n");
         }
     }
 }
